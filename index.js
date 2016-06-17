2309edd5c147c77e73cf1e8512f6ffe66aad399e
hippoom <hippoom@gmail.com>
2016-06-18 00:42:14 +0800

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+module.exports = function (kibana) {
+  return new kibana.Plugin({
+
+    uiExports: {
+      app: {
+        title: 'Kibana Guard',
+        description: 'A Kibana plugin for authentication and authorization',
+        main: 'plugins/kibana-guard-plugin/app'
+      }
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+init(server, options) {
+
+    }
+
+  });
+};
